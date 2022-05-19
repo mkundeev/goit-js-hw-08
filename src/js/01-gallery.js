10e4336759efce2e1f017dd030dbb8b57490604d
MaksymKundeev <mkundeev@gmail.com>
2022-05-19 23:38:41 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+function creatImg({ preview, description, original }) {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}
+
+function creatGallery(images) {
+    const gallery = images.map(creatImg);
+    return gallery.join('');
+}
+
+function addGallery(gallery) {
+   galleryEl.innerHTML = creatGallery(gallery); 
+}
+
+addGallery(galleryItems);
+
+new SimpleLightbox('.gallery__item', { captionsData: "alt", captionDelay: "250"});
