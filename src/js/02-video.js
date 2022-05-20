5ce0c14602826ab0c668b7a65c578e6b24398823
MaksymKundeev <mkundeev@gmail.com>
2022-05-20 22:29:31 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,14 @@
-   import throttle from 'lodash/throttle'
-   
-    const iframe = document.querySelector('#vimeo-player');
-    const player = new Vimeo.Player(iframe);
-   
-    const onPlay = function({seconds}) {
-    localStorage.setItem("videoplayer-current-time", seconds)
-    };
-    player.on('timeupdate', throttle(onPlay,1000));
+import throttle from 'lodash/throttle'
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function ({ seconds }) {
+   localStorage.setItem("videoplayer-current-time", seconds)
+};
+player.on('timeupdate', throttle(onPlay, 1000));
 
 
 
-    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
