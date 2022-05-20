775420f5a718540794a629ddcfa0edb593235ee3
MaksymKundeev <mkundeev@gmail.com>
2022-05-20 16:59:57 +0300

    2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,21 +2,35 @@ import throttle from 'lodash/throttle'
 
 const formEl = document.querySelector('.feedback-form');
 const obj = {};
+const STORAGE_KEY = "feedback-form-state";
 
-localStorage.setItem("feedback-form-state", JSON.stringify({}));
 
 formEl.addEventListener('input', throttle(onInputChange, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
 
-function onInputChange(evt) {
-    obj[evt.target.name] = evt.target.value;
 
-    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+populateForm();
+
+function onInputChange(evt) {
+    
+    obj[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(obj));
 }
 function onFormSubmit(evt) {
-    preventDefault();
-    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-    localStorage.setItem("feedback-form-state", JSON.stringify({}));
+    evt.preventDefault();
+    
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
+    localStorage.removeItem(STORAGE_KEY);
+    
+    evt.currentTarget.reset()
+}
 
+function populateForm() {
+    const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (saveData) {
+        for (let key of Object.keys(saveData)) {
+            formEl[key].value = saveData[key]
+        }
+    }
 }
